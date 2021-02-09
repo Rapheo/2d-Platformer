990e8accb4f087d184ffd8619a421f688be1b15d
Rapheo <rapheo.abdullah20@gmail.com>
2021-02-09 07:53:57 +0600

    ,

---
+++ Assets/RobbiePlatformer_Project/Assets/Tilemaps/Scripts/Player_movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //movement
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
